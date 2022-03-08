2c73987177d1fa86650d91d6c1d56e960038b1b4
Sixshaman <Shaman1234@gmail.com>
2022-03-09 00:05:36 +0300

    Web closures "fix"

--- src/event_loop_web.rs
+++ src/event_loop_web.rs
@@ -30,9 +30,6 @@ pub async fn run_event_loop()
     let initial_state_select       = document.get_element_by_id("initial_states").unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap();
     let initial_state_upload_input = document.get_element_by_id("board_input").unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap();
 
-    let board_upload_image = web_sys::HtmlImageElement::new().unwrap();
-    let board_file_reader  = web_sys::FileReader::new().unwrap();
-
 
     //Initializing the state
     let initial_width  = 1023;
@@ -49,164 +46,33 @@ pub async fn run_event_loop()
     stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Corners);
     stafra_state.reset_click_rule();
 
-    //Canvas resize handler
-    let stafra_state_clone_for_resize = stafra_state_rc.clone();
-    main_canvas.set_onresize(Some(Closure::wrap(Box::new(move |event: web_sys::Event|
-    {
-        let mut stafra_state = stafra_state_clone_for_resize.borrow_mut();
 
-        let canvas = event.target().unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
-        stafra_state.resize(canvas.client_width() as u32, canvas.client_height() as u32);
-    }) as Box<dyn Fn(web_sys::Event)>).as_ref().unchecked_ref()));
+    //Creating closures
+    let canvas_resize_closure = create_canvas_resize_closure(stafra_state_rc.clone());
 
-    //Save png handler
-    let stafra_state_clone_for_save_png = stafra_state_rc.clone();
-    save_png_button.set_onclick(Some(Closure::wrap(Box::new(move ||
-    {
-        let mut stafra_state = stafra_state_clone_for_save_png.borrow_mut();
-        stafra_state.post_png_data_request();
-    }) as Box<dyn Fn()>).as_ref().unchecked_ref()));
+    let save_png_closure = create_save_png_closure(stafra_state_rc.clone());
 
-    //Play/pause handler
-    let app_state_clone_for_play_pause = app_state_rc.clone();
-    play_pause_button.set_onclick(Some(Closure::wrap(Box::new(move ||
-    {
-        let mut app_state = app_state_clone_for_play_pause.borrow_mut();
+    let play_pause_closure = create_play_pause_closure(app_state_rc.clone());
+    let stop_closure       = create_stop_closure(app_state_rc.clone(), stafra_state_rc.clone());
+    let next_frame_closure = create_next_frame_closure(app_state_rc.clone(), stafra_state_rc.clone());
 
-        app_state.run_state = if app_state.run_state == RunState::Running {RunState::Paused} else {RunState::Running};
-        update_ui(&app_state.run_state);
-    }) as Box<dyn Fn()>).as_ref().unchecked_ref()));
+    let initial_state_upload_input_closure = create_board_upload_input_closure(app_state_rc.clone(), stafra_state_rc.clone());
 
-    //Stop handler
-    let app_state_clone_for_stop = app_state_rc.clone();
-    let stafra_state_clone_for_stop = stafra_state_rc.clone();
-    stop_button.set_onclick(Some(Closure::wrap(Box::new(move ||
-    {
-        let mut app_state  = app_state_clone_for_stop.borrow_mut();
-        let mut stafra_state = stafra_state_clone_for_stop.borrow_mut();
+    let select_initial_state_closure = create_select_initial_state_closure(app_state_rc.clone(), stafra_state_rc.clone());
 
-        app_state.run_state = RunState::Stopped;
-        stafra_state.reset_board_unchanged();
 
-        update_ui(&app_state.run_state);
-    }) as Box<dyn Fn()>).as_ref().unchecked_ref()));
+    //Setting closures
+    main_canvas.set_onresize(Some(canvas_resize_closure.as_ref().unchecked_ref()));
 
-    //Next frame handler
-    let app_state_clone_for_next_frame = app_state_rc.clone();
-    let stafra_state_clone_for_next_frame = stafra_state_rc.clone();
-    next_frame_button.set_onclick(Some(Closure::wrap(Box::new(move ||
-    {
-        let app_state = app_state_clone_for_next_frame.borrow();
-        let mut stafra_state = stafra_state_clone_for_next_frame.borrow_mut();
+    save_png_button.set_onclick(Some(save_png_closure.as_ref().unchecked_ref()));
 
-        if app_state.run_state != RunState::Running
-        {
-            stafra_state.update();
-        }
-    }) as Box<dyn Fn()>).as_ref().unchecked_ref()));
+    play_pause_button.set_onclick(Some(play_pause_closure.as_ref().unchecked_ref()));
+    stop_button.set_onclick(Some(stop_closure.as_ref().unchecked_ref()));
+    next_frame_button.set_onclick(Some(next_frame_closure.as_ref().unchecked_ref()));
 
-    //Board upload handler
-    let app_state_clone_for_board_upload = app_state_rc.clone();
-    let stafra_state_clone_for_board_upload = stafra_state_rc.clone();
-    board_upload_image.set_onload(Some(Closure::wrap(Box::new(move |event: web_sys::Event|
-    {
-        let mut app_state  = app_state_clone_for_board_upload.borrow_mut();
-        let mut stafra_state = stafra_state_clone_for_board_upload.borrow_mut();
+    initial_state_upload_input.set_onchange(Some(initial_state_upload_input_closure.as_ref().unchecked_ref()));
 
-        let board_image = event.target().unwrap().dyn_into::<web_sys::HtmlImageElement>().unwrap();
-
-        let document       = web_sys::window().unwrap().document().unwrap();
-        let canvas_board   = document.create_element("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
-        let canvas_context = canvas_board.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
-
-        canvas_board.set_width(board_image.width());
-        canvas_board.set_height(board_image.height());
-
-        canvas_context.draw_image_with_html_image_element(&board_image, 0.0, 0.0).expect("Draw image error!");
-        let image_data = canvas_context.get_image_data(0.0, 0.0, board_image.width() as f64, board_image.height() as f64).unwrap();
-
-        stafra_state.reset_board_custom(image_data.data().to_vec(), image_data.width(), image_data.height());
-        app_state.run_state = RunState::Running;
-
-        canvas_board.remove();
-        update_ui(&app_state.run_state);
-    }) as Box<dyn Fn(web_sys::Event)>).as_ref().unchecked_ref()));
-
-    //Board file read handler
-    board_file_reader.set_onload(Some(Closure::wrap(Box::new(move |event: web_sys::Event|
-    {
-        let file_reader      = event.target().unwrap().dyn_into::<web_sys::FileReader>().unwrap();
-        let file_read_result = file_reader.result().unwrap();
-
-        let file_data = file_read_result.as_string().unwrap();
-        board_upload_image.set_src(&file_data);
-    }) as Box<dyn Fn(web_sys::Event)>).as_ref().unchecked_ref()));
-
-    //Custom initial state upload handler
-    initial_state_upload_input.set_onchange(Some(Closure::wrap(Box::new(move |event: web_sys::Event|
-    {
-        let input_files = event.target().unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().files().unwrap();
-        if input_files.length() > 0
-        {
-            let image_file = input_files.item(0).unwrap();
-            let filename   = image_file.name();
-
-            //Change the select option text
-            let initial_state_select = document.get_element_by_id("initial_states").unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap();
-            let custom_option_index  = find_select_option_index(&initial_state_select, "initial_state_custom_value");
-
-            let initial_state_options = initial_state_select.options();
-            let custom_option_element = initial_state_options.item(custom_option_index.try_into().unwrap()).unwrap().dyn_into::<web_sys::HtmlOptionElement>().unwrap();
-
-            custom_option_element.set_text(&filename);
-            initial_state_select.set_selected_index(custom_option_index);
-
-            //Read the data
-            board_file_reader.read_as_data_url(&image_file).expect("Read data url error!");
-        }
-    }) as Box<dyn Fn(web_sys::Event)>).as_ref().unchecked_ref()));
-
-    //Reset board
-    let app_state_clone_for_initial_state_select = app_state_rc.clone();
-    let stafra_state_clone_for_initial_state_select = stafra_state_rc.clone();
-    initial_state_select.set_onchange(Some(Closure::wrap(Box::new(move |event: web_sys::Event|
-    {
-        let mut app_state  = app_state_clone_for_initial_state_select.borrow_mut();
-        let mut stafra_state = stafra_state_clone_for_initial_state_select.borrow_mut();
-
-        let board_reset_select = event.target().unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap();
-        match board_reset_select.value().as_str()
-        {
-            "initial_state_corners" =>
-            {
-                stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Corners);
-                app_state.run_state = RunState::Running;
-            },
-
-            "initial_state_sides" =>
-            {
-                stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Edges);
-                app_state.run_state = RunState::Running;
-            },
-
-            "initial_state_center" =>
-            {
-                stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Center);
-                app_state.run_state = RunState::Running;
-            },
-
-            "initial_state_custom" =>
-            {
-                let document = web_sys::window().unwrap().document().unwrap();
-                let initial_state_upload_input = document.get_element_by_id("board_input").unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap();
-                initial_state_upload_input.click();
-            },
-
-            _ => {}
-        }
-
-        update_ui(&app_state.run_state);
-    }) as Box<dyn FnMut(web_sys::Event)>).as_ref().unchecked_ref()));
+    initial_state_select.set_onchange(Some(select_initial_state_closure.as_ref().unchecked_ref()));
 
 
     //Refresh handler
@@ -273,6 +139,188 @@ pub async fn run_event_loop()
 
     update_ui(&app_state.run_state);
     window.request_animation_frame(refresh_function_copy.borrow().as_ref().unwrap().as_ref().unchecked_ref()).expect("Request animation frame error!");
+
+
+    //Forget the closures to keep them alive
+    canvas_resize_closure.forget();
+    save_png_closure.forget();
+    play_pause_closure.forget();
+    stop_closure.forget();
+    next_frame_closure.forget();
+    initial_state_upload_input_closure.forget();
+    select_initial_state_closure.forget();
+}
+
+fn create_canvas_resize_closure(stafra_state_rc: Rc<RefCell<stafra_state::StafraState>>) -> Closure<dyn Fn(web_sys::Event)>
+{
+    Closure::wrap(Box::new(move |event: web_sys::Event|
+    {
+        let mut stafra_state = stafra_state_rc.borrow_mut();
+
+        let canvas = event.target().unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+        stafra_state.resize(canvas.client_width() as u32, canvas.client_height() as u32);
+    }) as Box<dyn Fn(web_sys::Event)>)
+}
+
+fn create_save_png_closure(stafra_state_rc: Rc<RefCell<stafra_state::StafraState>>) -> Closure<dyn Fn()>
+{
+    Closure::wrap(Box::new(move ||
+    {
+        let mut stafra_state = stafra_state_rc.borrow_mut();
+        stafra_state.post_png_data_request();
+    })
+    as Box<dyn Fn()>)
+}
+
+fn create_play_pause_closure(app_state_rc: Rc<RefCell<app_state::AppState>>) -> Closure<dyn Fn()>
+{
+    Closure::wrap(Box::new(move ||
+    {
+        let mut app_state = app_state_rc.borrow_mut();
+
+        app_state.run_state = if app_state.run_state == RunState::Running {RunState::Paused} else {RunState::Running};
+        update_ui(&app_state.run_state);
+    }) as Box<dyn Fn()>)
+}
+
+fn create_stop_closure(app_state_rc: Rc<RefCell<app_state::AppState>>, stafra_state_rc: Rc<RefCell<stafra_state::StafraState>>) -> Closure<dyn Fn()>
+{
+    Closure::wrap(Box::new(move ||
+    {
+        let mut app_state    = app_state_rc.borrow_mut();
+        let mut stafra_state = stafra_state_rc.borrow_mut();
+
+        app_state.run_state = RunState::Stopped;
+        stafra_state.reset_board_unchanged();
+
+        update_ui(&app_state.run_state);
+    }) as Box<dyn Fn()>)
+}
+
+fn create_next_frame_closure(app_state_rc: Rc<RefCell<app_state::AppState>>, stafra_state_rc: Rc<RefCell<stafra_state::StafraState>>) -> Closure<dyn Fn()>
+{
+    Closure::wrap(Box::new(move ||
+    {
+        let     app_state    = app_state_rc.borrow();
+        let mut stafra_state = stafra_state_rc.borrow_mut();
+
+        if app_state.run_state != RunState::Running
+        {
+            stafra_state.update();
+        }
+    }) as Box<dyn Fn()>)
+}
+
+fn create_board_upload_input_closure(app_state_rc: Rc<RefCell<app_state::AppState>>, stafra_state_rc: Rc<RefCell<stafra_state::StafraState>>) -> Closure<dyn Fn(web_sys::Event)>
+{
+    let board_upload_image_closure = Closure::wrap(Box::new(move |event: web_sys::Event|
+    {
+        let mut app_state    = app_state_rc.borrow_mut();
+        let mut stafra_state = stafra_state_rc.borrow_mut();
+
+        let board_image = event.target().unwrap().dyn_into::<web_sys::HtmlImageElement>().unwrap();
+
+        let document       = web_sys::window().unwrap().document().unwrap();
+        let canvas_board   = document.create_element("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+        let canvas_context = canvas_board.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
+
+        canvas_board.set_width(board_image.width());
+        canvas_board.set_height(board_image.height());
+
+        canvas_context.draw_image_with_html_image_element(&board_image, 0.0, 0.0).expect("Draw image error!");
+        let image_data = canvas_context.get_image_data(0.0, 0.0, board_image.width() as f64, board_image.height() as f64).unwrap();
+
+        stafra_state.reset_board_custom(image_data.data().to_vec(), image_data.width(), image_data.height());
+        app_state.run_state = RunState::Running;
+
+        canvas_board.remove();
+        update_ui(&app_state.run_state);
+    }) as Box<dyn Fn(web_sys::Event)>);
+
+    let board_upload_image_element = web_sys::HtmlImageElement::new().unwrap();
+    board_upload_image_element.set_onload(Some(board_upload_image_closure.as_ref().unchecked_ref()));
+
+    let board_file_read_closure = Closure::wrap(Box::new(move |event: web_sys::Event|
+    {
+        let file_reader      = event.target().unwrap().dyn_into::<web_sys::FileReader>().unwrap();
+        let file_read_result = file_reader.result().unwrap();
+
+        let file_data = file_read_result.as_string().unwrap();
+        board_upload_image_element.set_src(&file_data);
+    }) as Box<dyn Fn(web_sys::Event)>);
+
+    let board_file_reader = web_sys::FileReader::new().unwrap();
+    board_file_reader.set_onload(Some(board_file_read_closure.as_ref().unchecked_ref()));
+
+    board_file_read_closure.forget();
+    board_upload_image_closure.forget();
+
+    Closure::wrap(Box::new(move |event: web_sys::Event|
+    {
+        let input_files = event.target().unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().files().unwrap();
+        if input_files.length() > 0
+        {
+            let document = web_sys::window().unwrap().document().unwrap();
+
+            let image_file = input_files.item(0).unwrap();
+            let filename   = image_file.name();
+
+            //Change the select option text
+            let initial_state_select = document.get_element_by_id("initial_states").unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap();
+            let custom_option_index  = find_select_option_index(&initial_state_select, "initial_state_custom_value");
+
+            let initial_state_options = initial_state_select.options();
+            let custom_option_element = initial_state_options.item(custom_option_index.try_into().unwrap()).unwrap().dyn_into::<web_sys::HtmlOptionElement>().unwrap();
+
+            custom_option_element.set_text(&filename);
+            initial_state_select.set_selected_index(custom_option_index);
+
+            //Read the data
+            board_file_reader.read_as_data_url(&image_file).expect("Read data url error!");
+        }
+    }) as Box<dyn Fn(web_sys::Event)>)
+}
+
+fn create_select_initial_state_closure(app_state_rc: Rc<RefCell<app_state::AppState>>, stafra_state_rc: Rc<RefCell<stafra_state::StafraState>>) -> Closure<dyn Fn(web_sys::Event)>
+{
+    Closure::wrap(Box::new(move |event: web_sys::Event|
+    {
+        let mut app_state    = app_state_rc.borrow_mut();
+        let mut stafra_state = stafra_state_rc.borrow_mut();
+
+        let board_reset_select = event.target().unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap();
+        match board_reset_select.value().as_str()
+        {
+            "initial_state_corners" =>
+            {
+                stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Corners);
+                app_state.run_state = RunState::Running;
+            },
+
+            "initial_state_sides" =>
+            {
+                stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Edges);
+                app_state.run_state = RunState::Running;
+            },
+
+            "initial_state_center" =>
+            {
+                stafra_state.reset_board_standard(stafra_state::StandardResetBoardType::Center);
+                app_state.run_state = RunState::Running;
+            },
+
+            "initial_state_custom" =>
+            {
+                let document = web_sys::window().unwrap().document().unwrap();
+                let initial_state_upload_input = document.get_element_by_id("board_input").unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap();
+                initial_state_upload_input.click();
+            },
+
+            _ => {}
+        }
+
+        update_ui(&app_state.run_state);
+    }) as Box<dyn Fn(web_sys::Event)>)
 }
 
 fn save_image_data(image_data: web_sys::ImageData, width: u32, height: u32)
